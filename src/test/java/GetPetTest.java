5a25580252e704b727629c79a837d6de91c2b21e
Serhii Klymenko <klymenkosergey87@gmail.com>
2020-03-10 13:42:22 +0200

    COVID-19 first rest test

---
+++ src/test/java/GetPetTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class GetPetTest {
+
+    @Test
+    public void getPetById() {
+        int id = 0;
+        given()
+                .log()
+                .all()
+                .baseUri("https://petstore.swagger.io")
+                .when()
+                .get( "/v2/pet/{id}", id)
+                .then()
+                .log()
+                .all()
+                .statusCode(404);
+    }
+
+    @Test
+    public void createPet () {
+
+    }
+}
