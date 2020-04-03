af3ba1827f71a7a3f7b0973e307766cefd826830
Serhii Klymenko <serhii_klymenko@Serhiis-MacBook-Pro.local>
2020-04-03 22:57:53 +0300

    create-before, delete-after

--- src/test/java/PetTest.java
+++ src/test/java/PetTest.java
@@ -11,25 +11,8 @@ import static io.restassured.filter.log.LogDetail.*;
 import static org.hamcrest.Matchers.*;
 
 public class PetTest {
-    int id = 228;
-    String body = "{\n" +
-            "  \"id\": \"" + id + "\",\n" +
-            "  \"category\": {\n" +
-            "    \"id\": 0,\n" +
-            "    \"name\": \"string\"\n" +
-            "  },\n" +
-            "  \"name\": \"UmaTurman\",\n" +
-            "  \"photoUrls\": [\n" +
-            "    \"string\"\n" +
-            "  ],\n" +
-            "  \"tags\": [\n" +
-            "    {\n" +
-            "      \"id\": 0,\n" +
-            "      \"name\": \"string\"\n" +
-            "    }\n" +
-            "  ],\n" +
-            "  \"status\": \"available\"\n" +
-            "}";
+
+int id = 228;
 
     @Before
     public void before() {
@@ -38,10 +21,12 @@ public class PetTest {
         spec.addHeader("Content-Type", "application/json");
         spec.log(ALL);
         RestAssured.requestSpecification = spec.build();
-///
+
         ResponseSpecBuilder response = new ResponseSpecBuilder();
         response.expectStatusCode(200);
         RestAssured.responseSpecification = response.build();
+
+        addNewPetToStore();
     }
 
     @After
@@ -52,16 +37,11 @@ public class PetTest {
     @Test
     public void getPetById() {
         given()
-                .body(body)
-                .when()
-                .post("/pet")
-                .then()
-                .body("id", is(id))
-                .statusCode(200);
-        given()
                 .when()
                 .get("/pet/{id}", id)
                 .then()
+                .log()
+                .all()
                 .body("id", anyOf(is(id), is("available")));
     }
 
@@ -69,8 +49,6 @@ public class PetTest {
     public void getPetByStatusAvailable() {
         String status = "available";
         given()
-                .log()
-                .all()
                 .when()
                 .get("/pet/findByStatus?status={status}", status)
                 .then()
@@ -83,23 +61,18 @@ public class PetTest {
     public void getPetByStatusSold() {
         String status = "sold";
         given()
-                .log()
-                .all()
                 .when()
                 .get("/pet/findByStatus?status={status}", status)
                 .then()
                 .log()
                 .all()
-                .body("status", everyItem(equalTo(status)))
-                .statusCode(200);
+                .body("status", everyItem(equalTo(status)));
     }
 
     @Test
     public void getPetByStatusPending() {
         String status = "pending";
         given()
-                .log()
-                .all()
                 .when()
                 .get("/pet/findByStatus?status={status}", status)
                 .then()
@@ -110,9 +83,25 @@ public class PetTest {
 
     @Test
     public void addNewPetToStore() {
+        String body = "{\n" +
+                "  \"id\": \"" + id + "\",\n" +
+                "  \"category\": {\n" +
+                "    \"id\": 0,\n" +
+                "    \"name\": \"string\"\n" +
+                "  },\n" +
+                "  \"name\": \"UmaTurman\",\n" +
+                "  \"photoUrls\": [\n" +
+                "    \"string\"\n" +
+                "  ],\n" +
+                "  \"tags\": [\n" +
+                "    {\n" +
+                "      \"id\": 0,\n" +
+                "      \"name\": \"string\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"status\": \"available\"\n" +
+                "}";
         given()
-                .log()
-                .all()
                 .body(body)
                 .when()
                 .post("/pet")
@@ -125,17 +114,6 @@ public class PetTest {
     @Test
     public void updatePetByDataForm() {
         given()
-                .body(body)
-                .when()
-                .post("/pet")
-                .then()
-                .log()
-                .all()
-                .body("id", is(id))
-                .statusCode(200);
-        given()
-                .log()
-                .all()
                 .contentType("application/x-www-form-urlencoded")
                 .param("name", "Jackie")
                 .param("status", "Sold")
@@ -149,15 +127,6 @@ public class PetTest {
 
     @Test
     public void updateExistingPet() {
-        given()
-                .body(body)
-                .when()
-                .post("/pet")
-                .then()
-                .log()
-                .all()
-                .body("id", is(id))
-                .statusCode(200);
         String body = "{\n" +
                 "  \"id\": \"" + id + "\",\n" +
                 "  \"category\": {\n" +
@@ -177,38 +146,22 @@ public class PetTest {
                 "  \"status\": \"available\"\n" +
                 "}";
         given()
-                .log()
-                .all()
                 .body(body)
                 .when()
                 .put("/pet")
                 .then()
                 .log()
                 .all()
-                .body("name", anyOf(is(id), is("Homer")))
-                .statusCode(200);
+                .body("name", anyOf(is(id), is("Homer")));
     }
-
     @Test
     public void deletePetById() {
         given()
-                .body(body)
-                .when()
-                .post("/pet")
-                .then()
-                .log()
-                .all()
-                .body("id", is(id))
-                .statusCode(200);
-        given()
-                .log()
-                .all()
                 .when()
                 .delete("/pet/{id}", id)
                 .then()
                 .log()
                 .all()
-                .body("message", is(String.valueOf(id)))
-                .statusCode(200);
+                .body("message", is(String.valueOf(id)));
     }
 }
