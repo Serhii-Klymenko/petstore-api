397c863d0c6585b466486514e19b4ceb0b56abd1
Serhii Klymenko <serhii_klymenko@Serhiis-MacBook-Pro.local>
2020-04-10 15:41:50 +0300

    Add PetEndPoint class/ update tests

---
+++ src/test/java/UpdatePetByFormDataTest.java
@@ -0,0 +1,47 @@
+import io.restassured.response.ValidatableResponse;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UpdatePetByFormDataTest {
+
+    PetEndpoint petEndpoint = new PetEndpoint();
+
+    long petId;
+
+    @Before
+    public void createPet() {
+        int id = 0;
+        String body = "{\n" +
+                "  \"id\": \""+ id +"\",\n" +
+                "  \"category\": {\n" +
+                "    \"id\": 0,\n" +
+                "    \"name\": \"string\"\n" +
+                "  },\n" +
+                "  \"name\": \"UmaTurman\",\n" +
+                "  \"photoUrls\": [\n" +
+                "    \"string\"\n" +
+                "  ],\n" +
+                "  \"tags\": [\n" +
+                "    {\n" +
+                "      \"id\": 0,\n" +
+                "      \"name\": \"string\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"status\": \"available\"\n" +
+                "}";
+        ValidatableResponse response = petEndpoint.createPet(body);
+        petId = response.extract().path("id");
+    }
+
+    @After
+    public void deletePet() {
+        petEndpoint.deletePet(petId);
+    }
+
+    @Test
+    public void updatePetByDataForm() {
+        petEndpoint.updatePetByFormData(petId);
+    }
+
+}
